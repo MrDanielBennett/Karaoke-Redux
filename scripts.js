15a4c9fbf66eb44c3d08133bad716ae17014805c
Daniel Bennett and Robert Bruce <daniel@>
2019-02-25 11:45:41 -0800

    intial commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,120 @@
-const userClick = () => {
-  console.log('click');
+// LYRIC INFO
+const songList = {
+  1: "Baby bye bye bye, Bye bye,Baby bye bye bye, Bye bye,Baby bye bye bye, Bye bye,Baby bye bye bye, Bye bye,Baby bye bye bye, Bye bye,Baby bye bye bye, Bye bye,Baby bye bye bye, Bye bye, Baby bye bye bye, Bye bye, but it ain't no lie, Baby bye bye bye".split(', '),
+  2: "Twenty-five years and my life is still, Trying to get up that great big hill of hope, For a destination, I realized quickly when I knew I should, That the world was made up of this brotherhood of man, For whatever that means, And so I cry sometimes when I'm lying in bed, Just to get it all out what's in my head, And I, I am feeling a little peculiar, And so I wake in the morning and I step outside, And I take a deep breath and I get real high, and I Scream from the top of my lungs, What's going on?, And I say hey yeah yeah hey yeah yeah, I said hey what's going on?, And I say hey yeah yeah hey yeah yeah,I said hey what's going on?".split(', ')
+};
+
+// INITIAL REDUX STATE
+const initialState = {
+  currentSongId: null,
+  songsById: {
+    1: {
+      title: "Bye Bye Bye",
+      artist: "N'Sync",
+      songId: 1,
+      songArray: songList[1],
+      arrayPosition: 0,
+    },
+    2: {
+      title: "What's Goin' On",
+      artist: "Four Non-Blondes",
+      songId: 2,
+      songArray: songList[2],
+      arrayPosition: 0,
+    }
+  }
+};
+
+// REDUX REDUCER
+const lyricChangeReducer = (state = initialState.songsById, action) => {
+  switch (action.type) {
+    case 'NEXT_LYRIC':
+      let newArrayPosition = state.arrayPosition + 1;
+      let newState = {
+        songLyricsArray: state.songLyricsArray,
+        arrayPosition: newArrayPosition,
+      }
+      newSongByIdEntry = { ...state[action.currentSongId], arrayPosition: newArrayPosition }
+      return newState;
+    case 'RESTART_SONG':
+      newState = initialState;
+      return newState;
+    default:
+      return state;
+  }
 }
+
+// JEST TESTS + SETUP
+const { expect } = window;
+
+expect(lyricChangeReducer(initialState.songsById, { type: null })).toEqual(initialState.songsById);
+
+expect(lyricChangeReducer(initialState.songsById, { type: 'NEXT_LYRIC', currentSongId: 2 })).toEqual({
+  1: {
+    title: "Bye Bye Bye",
+    artist: "N'Sync",
+    songId: 1,
+    songArray: songList[1],
+    arrayPosition: 0,
+  },
+  2: {
+    title: "What's Goin' On",
+    artist: "Four Non-Blondes",
+    songId: 2,
+    songArray: songList[2],
+    arrayPosition: 1,
+  }
+});
+
+expect(lyricChangeReducer(initialState.songsById, { type: 'RESTART_SONG', currentSongId: 1 })).toEqual({
+  1: {
+    title: "Bye Bye Bye",
+    artist: "N'Sync",
+    songId: 1,
+    songArray: songList[1],
+    arrayPosition: 0,
+  },
+  2: {
+    title: "What's Goin' On",
+    artist: "Four Non-Blondes",
+    songId: 2,
+    songArray: songList[2],
+    arrayPosition: 0,
+  }
+});
+
+// REDUX STORE
+const { createStore } = Redux;
+const store = createStore(lyricChangeReducer);
+console.log(store.getState());
+
+// REDERING STATE IN DOM
+const renderLyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics');
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
+  const renderedLine = document.createTextNode(currentLine);
+  document.getElementById('lyrics').appendChild(renderedLine);
+}
+
+window.onload = function(){
+  renderLyrics();
+}
+
+// CLICK LISTENER
+const userClick = () => {
+  const userClick = () => {
+    const currentState = store.getState();
+    if (currentState.arrayPosition === currentState.songLyricsArray.length - 1) {
+      store.dispatch({ type: 'RESTART_SONG' } );
+    } else {
+      store.dispatch({ type: 'NEXT_LYRIC' } );
+    }
+  }
+  console.log(store.getState());
+}
+
+// SUBSCRIBE TO REDUX STORE
+store.subscribe(renderLyrics);
